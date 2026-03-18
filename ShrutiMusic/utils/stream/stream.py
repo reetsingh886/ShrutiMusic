995f9b389086b78f482a7062d315d0875b32947f
ᯓ‌𐌌།⃪᱂ ⚠️𝑻𝒉𝒂𝒌𝒖𝒓𓂃 <reetsingh274@gmail.com>
2026-03-19 03:14:57 +0530

    Update stream.py

--- ShrutiMusic/utils/stream/stream.py
+++ ShrutiMusic/utils/stream/stream.py
@@ -34,108 +34,67 @@ return
 if forceplay:
     await Nand.force_stop_stream(chat_id)
 
-if streamtype == "playlist":
-    msg = f"{_['play_19']}\n\n"
-    count = 0
+if streamtype == "youtube":
+    vidid = result["vidid"]
+    title = result["title"].title()
+    duration_min = result["duration_min"]
+    thumbnail = result["thumb"]
+    status = True if video else None
 
-    for search in result:
-        if int(count) == config.PLAYLIST_FETCH_LIMIT:
-            continue
-        try:
-            title, duration_min, duration_sec, thumbnail, vidid = await YouTube.details(
-                search, False if spotify else True
-            )
-        except:
-            continue
-
-        if str(duration_min) == "None":
-            continue
-        if duration_sec > config.DURATION_LIMIT:
-            continue
-
-        if await is_active_chat(chat_id):
-            await put_queue(
-                chat_id,
-                original_chat_id,
-                f"vid_{vidid}",
-                title,
-                duration_min,
-                user_name,
-                vidid,
-                user_id,
-                "video" if video else "audio",
-            )
-            position = len(db.get(chat_id)) - 1
-            count += 1
-            msg += f"{count}. {title[:70]}\n"
-            msg += f"{_['play_20']} {position}\n\n"
-        else:
-            if not forceplay:
-                db[chat_id] = []
-
-            status = True if video else None
-
-            try:
-                file_path, direct = await YouTube.download(
-                    vidid, mystic, video=status, videoid=True
-                )
-            except:
-                raise AssistantErr(_["play_14"])
-
-            await Nand.join_call(
-                chat_id,
-                original_chat_id,
-                file_path,
-                video=status,
-                image=thumbnail,
-            )
-
-            await put_queue(
-                chat_id,
-                original_chat_id,
-                file_path if direct else f"vid_{vidid}",
-                title,
-                duration_min,
-                user_name,
-                vidid,
-                user_id,
-                "video" if video else "audio",
-                forceplay=forceplay,
-            )
-
-            img = await gen_thumb(vidid)
-            button = stream_markup(_, chat_id)
-
-            run = await app.send_photo(
-                original_chat_id,
-                photo=img,
-                caption=_["stream_1"].format(
-                    f"https://t.me/{app.username}?start=info_{vidid}",
-                    title[:23],
-                    duration_min,
-                    user_name,
-                ),
-                reply_markup=InlineKeyboardMarkup(button),
-                has_spoiler=True
-            )
-
-            db[chat_id][0]["mystic"] = run
-            db[chat_id][0]["markup"] = "stream"
-
-    if count == 0:
-        return
-    else:
-        link = await NandBin(msg)
-        lines = msg.count("\n")
-        car = os.linesep.join(msg.split(os.linesep)[:17]) if lines >= 17 else msg
-
-        carbon = await Carbon.generate(car, randint(100, 10000000))
-        upl = close_markup(_)
-
-        return await app.send_photo(
-            original_chat_id,
-            photo=carbon,
-            caption=_["play_21"].format(position, link),
-            reply_markup=upl,
-            has_spoiler=True
+    try:
+        file_path, direct = await YouTube.download(
+            vidid, mystic, videoid=True, video=status
         )
+    except:
+        raise AssistantErr(_["play_14"])
+
+    if await is_active_chat(chat_id):
+        await put_queue(
+            chat_id,
+            original_chat_id,
+            file_path if direct else f"vid_{vidid}",
+            title,
+            duration_min,
+            user_name,
+            vidid,
+            user_id,
+            "video" if video else "audio",
+        )
+    else:
+        db[chat_id] = []
+
+        await Nand.join_call(
+            chat_id,
+            original_chat_id,
+            file_path,
+            video=status,
+            image=thumbnail,
+        )
+
+        await put_queue(
+            chat_id,
+            original_chat_id,
+            file_path,
+            title,
+            duration_min,
+            user_name,
+            vidid,
+            user_id,
+            "video" if video else "audio",
+        )
+
+        img = await gen_thumb(vidid)
+        button = stream_markup(_, chat_id)
+
+        await app.send_photo(
+            original_chat_id,
+            photo=img,
+            caption=_["stream_1"].format(
+                f"https://t.me/{app.username}?start=info_{vidid}",
+                title[:23],
+                duration_min,
+                user_name,
+            ),
+            reply_markup=InlineKeyboardMarkup(button),
+            has_spoiler=True
+        )      
